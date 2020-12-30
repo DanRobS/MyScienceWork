eb599c7251cfc8a797444441eaad3f84398eae76
DanRobS <roberttsoumbou@gmail.com>
2020-12-30 16:18:34 +0100

    adding icons to tabs

--- src/icons/icons.js
+++ src/icons/icons.js
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookSquare, faLinkedin, faTwitter, faOrcid
          } from '@fortawesome/free-brands-svg-icons'
-import { faUpload } from '@fortawesome/free-solid-svg-icons'
+import { faUpload, faUser, faFileAlt, faHome } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faFacebookSquare, faLinkedin, faTwitter, faOrcid,
-        faUpload)
+        faUpload, faUser, faFileAlt, faHome)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
--- src/views/TabsUtilisateur.vue
+++ src/views/TabsUtilisateur.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="TabsUtilisateur column is-9">
     <div id="top-tabs" class="tabs is-toggle 
-              is-centered is-boxed is-large">
+              is-centered is-boxed is-large is-info"
+              type="is-toggle">
       <ul>
         <li>
             <router-link to="/vuegenerale">
+            <span class="icon">
+              <font-awesome-icon :icon="['fas', 'home']" size=""
+                :style="{ color: '#344b98' }"/>
+            </span>
               Vue generale
             </router-link>
         </li>
         <li>
             <router-link to="/profil">
+            <span class="icon">
+              <font-awesome-icon :icon="['fas', 'user']" size=""
+                :style="{ color: '#344b98' }"/>
+            </span>
               Mon profil
             </router-link>
         </li>
-        <li >
+        <li>
             <router-link to="/publications">
+            <span class="icon">
+              <font-awesome-icon :icon="['fas', 'file-alt']" size=""
+                :style="{ color: '#344b98' }"/>
+            </span>
               Mes publications
             </router-link>
         </li>
@@ -38,11 +51,7 @@ export default {
 .TabsUtilisateur {
   border-style: solid;
   border-color: black;
-  background-color: #d1d8ef;
-}
-
-#top-tabs {
-  
+  background-color: #a4b2e0;
 }
 
 @media (max-width: 768px) {
